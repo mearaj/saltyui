241e96ff4ef60034b3deea335942429ed50ba997
Mearaj Bhagad <mearajbhagad@gmail.com>
2022-04-20 01:44:48 +0530

    backup

--- ui/chat_room_page_item.go
+++ ui/chat_room_page_item.go
@@ -35,42 +35,46 @@ func (c *ChatRoomPageItem) Layout(gtx Gtx) (d Dim) {
 }
 
 func (c *ChatRoomPageItem) commonLayout(gtx Gtx, spacing layout.Spacing) Dim {
-	macro := op.Record(gtx.Ops)
-	inset := layout.UniformInset(unit.Dp(12))
-	d := inset.Layout(gtx, func(gtx Gtx) Dim {
-		flex := layout.Flex{Spacing: spacing, Axis: layout.Vertical}
-		return flex.Layout(gtx,
-			layout.Rigid(func(gtx Gtx) Dim {
-				timeVal, _ := time.Parse(time.RFC3339, c.Message.Created)
-				txtMsg := timeVal.Format("Mon Jan 2 15:04 2006")
-				label := material.Label(c.Theme, unit.Sp(12), txtMsg)
-				label.Color = color.NRGBA{
-					R: colornames.Gray.R,
-					G: colornames.Gray.G,
-					B: colornames.Gray.B,
-					A: colornames.Gray.A,
-				}
-				label.Font.Weight = text.Bold
-				label.Font.Style = text.Italic
-				return layout.Inset{
-					Bottom: unit.Dp(8.0),
-				}.Layout(gtx, func(gtx Gtx) Dim {
-					return component.TruncatingLabelStyle(label).Layout(gtx)
-				})
-			}),
-			layout.Rigid(func(gtx2 Gtx) Dim {
-				gtx.Constraints.Max.X = int(float32(gtx.Constraints.Max.X) / 1.5)
-				bd := material.Body1(c.Theme, c.Message.Text)
-				return bd.Layout(gtx)
-			}))
-	})
-	bgColor := c.Theme.ContrastBg
-	bgColor.A = 50
-	inset = layout.Inset{Top: unit.Dp(8), Bottom: unit.Dp(8)}
-	rect := component.Rect{Color: bgColor, Size: d.Size, Radii: unit.Dp(8).V}
-	call := macro.Stop()
-	defer call.Add(gtx.Ops)
-	return inset.Layout(gtx, func(gtx2 Gtx) Dim {
-		return rect.Layout(gtx)
-	})
+	d := layout.Flex{}.Layout(gtx,
+		layout.Rigid(func(gtx Gtx) Dim {
+			timeVal, _ := time.Parse(time.RFC3339, c.Message.Created)
+			txtMsg := timeVal.Format("Mon Jan 2 15:04 2006")
+			label := material.Label(c.Theme, unit.Sp(12), txtMsg)
+			label.Color = color.NRGBA{
+				R: colornames.Gray.R,
+				G: colornames.Gray.G,
+				B: colornames.Gray.B,
+				A: colornames.Gray.A,
+			}
+			label.Font.Weight = text.Bold
+			label.Font.Style = text.Italic
+			return layout.Inset{
+				Bottom: unit.Dp(8.0),
+			}.Layout(gtx, func(gtx Gtx) Dim {
+				return component.TruncatingLabelStyle(label).Layout(gtx)
+			})
+		}),
+		layout.Rigid(func(gtx2 Gtx) Dim {
+			macro := op.Record(gtx.Ops)
+			inset := layout.UniformInset(unit.Dp(12))
+			d := inset.Layout(gtx, func(gtx Gtx) Dim {
+				flex := layout.Flex{Spacing: spacing, Axis: layout.Vertical}
+				return flex.Layout(gtx,
+					layout.Rigid(func(gtx2 Gtx) Dim {
+						gtx.Constraints.Max.X = int(float32(gtx.Constraints.Max.X) / 1.5)
+						bd := material.Body1(c.Theme, c.Message.Text)
+						return bd.Layout(gtx)
+					}))
+			})
+			bgColor := c.Theme.ContrastBg
+			bgColor.A = 50
+			inset = layout.Inset{Top: unit.Dp(8), Bottom: unit.Dp(8)}
+			rect := component.Rect{Color: bgColor, Size: d.Size, Radii: unit.Dp(8).V}
+			call := macro.Stop()
+			defer call.Add(gtx.Ops)
+			return inset.Layout(gtx, func(gtx2 Gtx) Dim {
+				return rect.Layout(gtx)
+			})
+		}))
+	return d
 }
