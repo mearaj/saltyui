8c651786efec4dd26dac1ecb2630b76d21757a60
James Mills <prologic@shortcircuit.net.au>
2022-04-23 11:50:11 +1000

    Add iOS App Bundle

---
+++ ios/saltychat/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by James Mills on 23/4/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/saltychat/Shared/saltychatApp.swift
@@ -0,0 +1,17 @@
+//
+//  saltychatApp.swift
+//  Shared
+//
+//  Created by James Mills on 23/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct saltychatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
